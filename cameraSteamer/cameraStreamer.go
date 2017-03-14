d1ab9f70d773d51dcee5c7f8a8bb1ff87cda7753
Cole Smith <css459@nyu.edu>
2017-03-13 21:27:35 -0400

    added camera streamer program

---
+++ cameraSteamer/cameraStreamer.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"fmt"
+	"image/png"
+	"net"
+	"os"
+	"strings"
+)
+
+const (
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+var address string
+
+func handleRequest(conn net.Conn) {
+	for {
+
+		// Get base64 string from TCP Client
+		str, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
+			fmt.Println("Closing Connection")
+			return
+		}
+
+		// Remove whitespace and decode into jpeg image
+		str = strings.TrimSpace(str)
+		reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(str))
+		_, err = png.Decode(reader)
+		if err != nil {
+			fmt.Println("ERR:", err)
+		} else {
+			fmt.Println("Got Image")
+		}
+	}
+
+	fmt.Println("Closing Connection")
+	return
+}
+
+func main() {
+
+	// Get IP Address
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Exit(1)
+	}
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				//os.Stdout.WriteString(ipnet.IP.String() + "\n")
+				address = ipnet.IP.String()
+			}
+		}
+	}
+
+	// Begin listening
+	listen, err := net.Listen(CONN_TYPE, address+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error establishing listener: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("TCP listener open on ", address, ":", CONN_PORT)
+	defer listen.Close()
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("New Connection: ", conn)
+		go handleRequest(conn)
+	}
+}
