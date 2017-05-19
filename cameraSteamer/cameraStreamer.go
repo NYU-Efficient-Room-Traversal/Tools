b51b37484a3a68b0f19ba73f1a076335c69e066c
Cole Smith <css459@nyu.edu>
2017-05-19 17:32:46 -0400

    made cameraStreamer into a lib

--- cameraSteamer/cameraStreamer.go
+++ cameraSteamer/cameraStreamer.go
@@ -1,9 +1,10 @@
-package main
+package cameraStreamer // import github.com/NYU-Efficient-Room-Traversal/Tools/cameraStreamer
 
 import (
 	"bufio"
 	"encoding/base64"
 	"fmt"
+	"image"
 	"image/png"
 	"net"
 	"os"
@@ -17,7 +18,7 @@ const (
 
 var address string
 
-func handleRequest(conn net.Conn) {
+func handleRequest(conn net.Conn, ch chan<- image.Image) {
 	for {
 
 		// Get base64 string from TCP Client
@@ -31,11 +32,12 @@ func handleRequest(conn net.Conn) {
 		// Remove whitespace and decode into jpeg image
 		str = strings.TrimSpace(str)
 		reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(str))
-		_, err = png.Decode(reader)
+		img, err := png.Decode(reader)
 		if err != nil {
 			fmt.Println("ERR:", err)
 		} else {
 			fmt.Println("Got Image")
+			ch <- img
 		}
 	}
 
@@ -43,7 +45,9 @@ func handleRequest(conn net.Conn) {
 	return
 }
 
-func main() {
+func Open(ch chan<- image.Image) {
+	// Attach Channel
+	channel = ch
 
 	// Get IP Address
 	addrs, err := net.InterfaceAddrs()
@@ -53,7 +57,6 @@ func main() {
 	for _, a := range addrs {
 		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				//os.Stdout.WriteString(ipnet.IP.String() + "\n")
 				address = ipnet.IP.String()
 			}
 		}
@@ -75,6 +78,17 @@ func main() {
 		}
 
 		fmt.Println("New Connection: ", conn)
-		go handleRequest(conn)
+		go handleRequest(conn, ch)
+	}
+}
+
+func main() {
+	c := make(chan image.Image, 10)
+	go Open(c)
+	for {
+		i := <-c
+		if i != nil {
+			fmt.Println("get")
+		}
 	}
 }
