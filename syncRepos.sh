69a415db5f2bfabd6d4227174cb2fcdf18303a1e
Cole Smith <css459@nyu.edu>
2017-07-13 19:04:14 -0400

    added tool to sync all repos

---
+++ syncRepos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+go install github.com/NYU-Efficient-Room-Traversal/Roomba-Control
+
+go install github.com/NYU-Efficient-Room-Traversal/Tools/...
+
+go install github.com/NYU-Efficient-Room-Traversal/Rangefinder
+
+go install github.com/NYU-Efficient-Room-Traversal/Traverser
