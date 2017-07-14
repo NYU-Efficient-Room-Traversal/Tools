f2957c3a8a60e9d8e94d7b1c1919ab3b4129082a
Cole Smith <css459@nyu.edu>
2017-07-14 15:41:01 -0400

    added support for hue filtering

--- imagePreview/imagePreview.go
+++ imagePreview/imagePreview.go
@@ -12,22 +12,41 @@ import (
 
 var ch chan image.Image
 
+const VAL_THRESHOLD = 0.00
+const HUE_THRESHOLD = 20.0
+const HUE_TARGET = 332.0
+
 func handler(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("Image Requested")
 
 	img := <-ch
 
-	//rgba, ok := img.(*image.RGBA)
-	//if !ok {
-	//fmt.Println("Type Assertion for RGBA image failed")
-	//return
-	//}
+	rgba, ok := img.(*image.RGBA)
+	if !ok {
+		fmt.Println("Type Assertion for RGBA image failed")
+		return
+	}
 
-	//mat := rangefinder.NewMonoImageMatrix(rgba, 0.50)
+	// Convert to Matrix
+	imageMatrix := rangefinder.NewImageMatrix(rgba)
+
+	// Filter by hue
+	hueFiltered := imageMatrix.ConvertToMonoImageMatrixFromHue(HUE_TARGET, HUE_THRESHOLD)
+
+	// Filter by value
+	valFiltered := imageMatrix.ConvertToMonoImageMatrixFromValue(VAL_THRESHOLD)
+
+	// Intersect
+	intersectFiltered, err := rangefinder.GetMonoIntersectMatrix(hueFiltered, valFiltered)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//mat := rangefinder.NewMonoImageMatrix(rgba, THRESHOLD)
 
 	res.Header().Set("Content-Type", "image/png")
-	//err := png.Encode(res, matToImage(mat))
-	err := png.Encode(res, img)
+	err = png.Encode(res, matToImage(intersectFiltered))
+	//err := png.Encode(res, img)
 	if err != nil {
 		fmt.Println("PNG ENCODE ERROR: %v", err)
 		return
